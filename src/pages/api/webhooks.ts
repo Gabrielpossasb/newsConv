e1c124f56b5eb0eea78a0c8818adbc67231ffc49
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-12-06 17:38:28 -0400

    buy-stripe-success

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,20 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { Readable } from 'stream'
 
-async function buffer(readable: Readable) {
-   const chunks = [];
 
-   for await (const chunk of readable) {
-      chunks.push(
-         typeof chunk === "string" ? Buffer.concat(chunk) : chunk
-      );
-   }
-
-   return Buffer.concat(chunks);
-}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-   const buf = await buffer(req)
 
    res.status(200).json({ ok: true})
 }
